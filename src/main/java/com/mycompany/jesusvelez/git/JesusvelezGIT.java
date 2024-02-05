fc1e1d6c4eedffc1609d83c579502b062c46a895
jesus <jesus@localhost>
2024-02-05 18:54:52 +0100

    cambio2

--- src/main/java/com/mycompany/jesusvelez/git/JesusvelezGIT.java
+++ src/main/java/com/mycompany/jesusvelez/git/JesusvelezGIT.java
@@ -11,6 +11,6 @@ package com.mycompany.jesusvelez.git;
 public class JesusvelezGIT {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo de la Progamacion");
     }
 }
